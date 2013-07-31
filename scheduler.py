e88cc74b360271f79175b93f60552d1a94c6aaad
Goldman <brianwgoldman@acm.org>
2013-07-31 16:48:48 -0400

    Scheduler now runs until it times out, cleans up .start files

--- scheduler.py
+++ scheduler.py
@@ -1,7 +1,14 @@
 import sys
 import glob
-from os import path
+from os import path, remove
 from subprocess import call
+
+import signal
+
+def signal_handler(signal, frame):
+        raise Exception('Signal received, exiting')
+signal.signal(signal.SIGINT, signal_handler)
+
 
 executable = path.join("Release", "P3")
 folder = sys.argv[1]
@@ -9,17 +16,10 @@ repeats = int(sys.argv[2])
 bound_file = path.join(folder, "bounds.txt")
 arguments = path.join("config", "default.txt ") + " ".join(sys.argv[3:])
 single_run = "-experiment single_run"
-try:
-    with open(bound_file, 'r') as f:
-        least, most, maxed = map(int, f.read().strip().split())
-except (IOError, ValueError):
-    least, most, maxed = 0, 1, 0
-
 
 def update(least, most, maxed):
     with open(bound_file, 'w') as f:
         f.write("%i %i %i\n" % (least, most, maxed))
-
 
 def examine_results(folder, repeats, pop_size):
     successes = 0
@@ -46,40 +46,54 @@ def start_run(folder, repeats, pop_size):
             extra = extra % (pop_size, filename, i, i)
             combined = " ".join([executable, arguments, single_run, extra])
             print combined
-            call(combined.split())
+            try:
+                call(combined.split())
+            except Exception as e:
+                remove(filename + ".start")
+                print e
+                sys.exit()
             return
     print "RUNS INCOMPLETE"
 
-while maxed == 0:
-    print "FIRST LOOP", least, most
-    pop_size = most
-    # Try and find a job at this level
-    status = examine_results(folder, repeats, pop_size)
-    if status == "failed":
-        least = most
-        most *= 2
-        update(least, most, maxed)
-    elif status == "finished":
-        maxed = 1
-        update(least, most, maxed)
-    else:  # incomplete
-        start_run(folder, repeats, pop_size)
-        break
 
-while maxed > 0 and least + 1 < most:
-    print "SECOND LOOP", least, most
-    pop_size = (most + least) / 2
-    status = examine_results(folder, repeats, pop_size)
-    if status == "failed":
-        least = pop_size
-        update(least, most, maxed)
-    elif status == "finished":
-        most = pop_size
-        update(least, most, maxed)
-    else:  # incomplete
-        start_run(folder, repeats, pop_size)
-        break
+while True:
+    try:
+        with open(bound_file, 'r') as f:
+            least, most, maxed = map(int, f.read().strip().split())
+    except (IOError, ValueError):
+        least, most, maxed = 0, 1, 0
 
-if maxed != 0 and least + 1 == most:
-    with open(path.join(folder, "result.txt"), 'w') as f:
-        f.write(" ".join([executable, arguments, '-pop_size %i' % most, '\n']))
+    while maxed == 0:
+        print "FIRST LOOP", least, most
+        pop_size = most
+        # Try and find a job at this level
+        status = examine_results(folder, repeats, pop_size)
+        if status == "failed":
+            least = most
+            most *= 2
+            update(least, most, maxed)
+        elif status == "finished":
+            maxed = 1
+            update(least, most, maxed)
+        else:  # incomplete
+            start_run(folder, repeats, pop_size)
+            break
+
+    while maxed > 0 and least + 1 < most:
+        print "SECOND LOOP", least, most
+        pop_size = (most + least) / 2
+        status = examine_results(folder, repeats, pop_size)
+        if status == "failed":
+            least = pop_size
+            update(least, most, maxed)
+        elif status == "finished":
+            most = pop_size
+            update(least, most, maxed)
+        else:  # incomplete
+            start_run(folder, repeats, pop_size)
+            break
+
+    if maxed != 0 and least + 1 == most:
+        with open(path.join(folder, "result.txt"), 'w') as f:
+            f.write(" ".join([executable, arguments, '-pop_size %i' % most, '\n']))
+        break
