3592ef7dddabd06e2a2c413dbb850965f0d4a4b9
Goldman <brianwgoldman@acm.org>
2013-07-31 11:38:25 -0400

    Script now runs with constant number of jobs instead of accordian

--- starter.sh
+++ starter.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
 name=$1
+jobs=$2
 echo $name
 
 mkdir -p $name
 
-cat sub.sub | sed -e "s,ARGUMENTS,$*,g" -e "s/NAME/$name/g"> $name/sub.sub
+sed -e "s,ARGUMENTS,$*,g" -e "s/NAME/$name/g" sub.sub > $name/sub.sub
+for i in `seq $jobs`
+do
+	echo Starting $i
+	qsub $name/sub.sub
+done
