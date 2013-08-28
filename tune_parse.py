c7f364c34d8c4309c8f7b321d5f5c7a896a5824c
Goldman <brianwgoldman@acm.org>
2013-08-28 14:27:26 -0400

    Bunch of changes to tuneparse, most of them probably bad

--- tune_parse.py
+++ tune_parse.py
@@ -1,19 +1,37 @@
 import sys
 from os import path
 from collections import defaultdict
+import numpy
 
 sortable = defaultdict(list)
 headers = ['median', 'mad', 'failures', 'hill_climber', 'cluster_ordering', 'no_singles',
            'donate_until_different', 'keep_zeros']
 
+table = defaultdict(dict)
+all_problems = set()
 for filename in sys.argv[1:]:
     parts = path.basename(filename).strip().split('.')
     problem = '_'.join(parts[0:2])
     version = parts[2:-1]
-
     with open(filename, 'r') as f:
-        median = map(int, f.read().split('\n')[0].split(' ')[1:])
-    sortable[problem].append(median + version)
+        # median = map(int, f.read().split('\n')[0].split(' ')[1:])
+        lines = f.read().strip().split('\n')
+        median = map(int, lines[0].split(' ')[1:])
+        values = [int(line.split(' ')[1]) for line in lines[1:]]
+        if min(values) < 0 or max(values) > 1000000000:
+            print filename, min(values), max(values)
+            continue
+        median[0] = numpy.mean(values)
+    # sortable[problem].append(median + version)
+    table[tuple(version)][problem] = median
+    all_problems.add(problem)
+
+for version, data in table.items():
+    if len(data) == len(all_problems):
+        for problem, median in data.items():
+            sortable[problem].append(median + list(version))
+        for problem in all_problems - set(data.keys()):
+            sortable[problem].append([sys.maxint, 0, 0] + list(version))
 
 ranks = [defaultdict(list) for _ in range(len(headers))]
 full = defaultdict(list)
@@ -38,5 +56,5 @@ for index in range(3, len(headers)):
 
 print '----- FULL -----'
 print problem_order
-for version, data in sorted(full.items(), key=lambda X: (-len(X[1]), max(X[1]))):
+for version, data in sorted(full.items(), key=lambda X: (-len(X[1]), sum(X[1]))):
     print '\t'.join(version), data, sum(data) / len(data)
