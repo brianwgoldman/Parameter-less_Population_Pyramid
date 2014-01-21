d1c93e2eabac9c3e72bce8f97301af8db0a34f7b
Goldman <brianwgoldman@acm.org>
2014-01-21 11:20:10 -0500

    Solution file now has format of: fitness evaluations solution

--- src/MiddleLayer.cpp
+++ src/MiddleLayer.cpp
@@ -16,7 +16,7 @@ float Middle_Layer::evaluate(const vector<bool>& solution) {
     best_found = evaluations;
     results.add(fitness, evaluations);
     std::ofstream output(outfile, std::fstream::app);
-    output << best_fitness << " ";
+    output << best_fitness << " " << evaluations << " ";
     print(best_solution, output);
   }
   return fitness;
